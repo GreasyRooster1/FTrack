2b9150fe17b41d24f45dcd4c42117cdeefb677fe
GreasyRooster <greasyRooster1@gmail.com>
2025-01-29 13:20:04 -0800

    mobile nav icon

--- src/App/Nav/MobileNavIcon.tsx
+++ src/App/Nav/MobileNavIcon.tsx
@@ -1,7 +1,13 @@
-function MobileNavIcon(props) {
-    return <>
+interface mobileNavIconProps{
+    href: string,
+    src: string
+}
 
-    </>
+function MobileNavIcon(props: mobileNavIconProps) {
+    return <a href = {props.href}>
+        <img src={props.src} alt="icon"></img>
+    </a>
 }
 
 export {MobileNavIcon};
+export type { mobileNavIconProps };
