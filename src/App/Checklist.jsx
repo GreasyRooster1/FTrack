b2174201b52c44e8dd6408300ffeea9fe2b82e1f
GreasyRooster <greasyRooster1@gmail.com>
2025-01-29 14:16:27 -0800

    checklist and ssfdsdfsdfwsd

--- src/App/Checklist.jsx
+++ src/App/Checklist.jsx
@@ -1,7 +1,21 @@
-function ChecklistPage() {
-    return <>
+function Checklist(props) {
+    const style = {
+        display: 'flex',
+        flexDirection: 'column',
 
-    </>
+    }
+    const headerStyle = {
+
+    }
+    const progressStyle = {
+
+    }
+    return <div style={style}>
+        <div style={headerStyle}>
+            <progress id="file" value="32" max="100" style={progressStyle}> 32%</progress>
+        </div>
+        {props.children}
+    </div>
 }
 
-export {ChecklistPage}
+export {Checklist}
--- src/App/ChecklistItem.jsx
+++ src/App/ChecklistItem.jsx
@@ -1,7 +1,12 @@
-function ChecklistItem() {
-    return <>
+import {useState} from "react";
 
-    </>
+function ChecklistItem(props) {
+    let [checked,setChecked] = useState(false);
+
+    return <div>
+        <span>{checked}</span>
+        <div>{props.children}</div>
+    </div>
 }
 
 export {ChecklistItem}
