f7d678a8d49592de98db566509371a0579535e3c
GreasyRooster <greasyRooster1@gmail.com>
2025-01-29 13:43:48 -0800

    d

--- src/App/MobileApp.jsx
+++ src/App/MobileApp.jsx
@@ -10,6 +10,8 @@ function MobileApp() {
     return <Router>
         <Routes>
             <Route exact path="/" element={<Index />} />
+            <Route path="/checklist" element={<Checklist />} />
+            <Route path="/streak" element={<Streak />} />
         </Routes>
         <MobileNavBar/>
     </Router>
